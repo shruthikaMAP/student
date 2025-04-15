ab5d58de218e1524a7905c119511bfe2eadc6ed6
shruthika <shruthika.1706@gmail.com>
2025-04-15 15:39:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+<script>
+const typingElement = document.querySelector(".typing");
+const roles = ["Frontend Developer", "Video Editor", "Creative Designer", "DTP Developer", "WordPress Developer"];
+</script>
